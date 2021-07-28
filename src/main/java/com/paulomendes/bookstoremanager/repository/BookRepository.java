5cbcc0d41461ddd8b6435d5a00f5c502b5d49b3e
Paulo Mendes <paulo.mendes@dbasuporte.com>
2021-07-28 14:49:47 -0300

    Created book repository class

---
+++ src/main/java/com/paulomendes/bookstoremanager/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.paulomendes.bookstoremanager.repository;
+
+import com.paulomendes.bookstoremanager.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+
+}
